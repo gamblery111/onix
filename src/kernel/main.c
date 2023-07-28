4c550af0994fad6a0e999f498b86d472e5f7852f
gamblery111 <y18503950913@163.com>
2023-07-28 21:38:10 +0800

    3-1

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -7,8 +7,9 @@ char buf[1024];
 void kernel_init()
 {
     char *video = (char *) 0xb8000;
-    for(int i = 0; i< sizeof(message); i++)
+    for( int i = 0; i< sizeof(message); i++)
     {
         video[i * 2] = message[i];
     }
 }
+
