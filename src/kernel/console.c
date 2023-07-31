9acc9a350b9e3b6f986efda07940f6078ce3bbb7
gamblery111 <y18503950913@163.com>
2023-07-31 20:03:06 +0800

    23-printfk

--- src/kernel/console.c
+++ src/kernel/console.c
@@ -34,7 +34,7 @@ static u32 screen; // 当前显示器开始的位置
 
 static u32 pos; // 记录当前光标的内存位置
 
-static x, y; // 当前光标的位置
+static u32 x, y; // 当前光标的位置
 
 static u8 attr = 7;        // 字符样式
 static u16 erase = 0x0720; // 空格
@@ -95,7 +95,7 @@ void console_clear()
     set_screen();
 
     u16 *ptr = (u16 *)MEM_BASE;
-    while (ptr < MEM_END)
+    while (ptr < (u16 *)MEM_END)
     {
         *ptr++ = erase;
     }
@@ -116,7 +116,7 @@ static void scroll_up()
     }
     else 
     {
-        memcpy(MEM_BASE, screen, SCR_SIZE);
+        memcpy((void *)MEM_BASE, (void *)screen, SCR_SIZE);
         pos -= (screen - MEM_BASE);
         screen = MEM_BASE;
     }
