bf8c797a78acabe7a9d62a0c63bea17672ef3fad
gamblery111 <y18503950913@163.com>
2023-08-04 19:58:22 +0800

    37-时间

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -15,6 +15,7 @@ extern void console_init();
 extern void gdt_init();
 extern void interrupt_init();
 extern void clock_init();
+extern void time_init();
 extern void hang();
 
 void kernel_init()
@@ -22,9 +23,10 @@ void kernel_init()
     console_init();
     gdt_init();
     interrupt_init();
-    // task_init();
     clock_init();
+    time_init();
 
+    // task_init();
     asm volatile("sti");
     hang();
 }
