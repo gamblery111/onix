a8567292addef76f19ae333ddc93337dd35af2ce
gamblery111 <y18503950913@163.com>
2023-08-04 14:20:29 +0800

    35-计时器和时钟

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -11,12 +11,20 @@
 #include <onix/interrupt.h>
 #include <onix/stdlib.h>
 
+extern void console_init();
+extern void gdt_init();
+extern void interrupt_init();
+extern void clock_init();
+extern void hang();
+
 void kernel_init()
 {
     console_init();
     gdt_init();
     interrupt_init();
-    task_init();
- 
-    return;
+    // task_init();
+    clock_init();
+
+    asm volatile("sti");
+    hang();
 }
