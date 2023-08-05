2fb65d3c4cae16a74e34d429943f6e7e42fcd177
gamblery111 <y18503950913@163.com>
2023-08-05 11:31:51 +0800

    40-物理内存管理

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -12,22 +12,25 @@
 #include <onix/stdlib.h>
 #include <onix/rtc.h>
 
-extern void console_init();
-extern void gdt_init();
+extern void memory_map_init();
 extern void interrupt_init();
 extern void clock_init();
 extern void time_init();
 extern void rtc_init();
 extern void hang();
 
+extern void memory_test();
+
 void kernel_init()
 {
-    console_init();
-    gdt_init();
+    memory_map_init();
     interrupt_init();
     clock_init();
-    time_init();
-    rtc_init();
+    // time_init();
+    // rtc_init();
+
+    memory_test();
+
     // task_init();
     asm volatile("sti");
     hang();
--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -1,6 +1,7 @@
 [bits 32]
 
 extern console_init
+extern gdt_init
 extern memory_init
 extern kernel_init
 
@@ -11,5 +12,9 @@ _start:
 
     call console_init; 控制台初始化
     call memory_init; 内存初始化
+    call console_init   ; 控制台初始化
+    call gdt_init       ; 全局描述符初始化
+    call memory_init    ; 内存初始化
+    call kernel_init    ; 内核初始化
 
     jmp $; 阻塞
