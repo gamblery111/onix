9350b137522024a774ca40cf65215c83d174cf60
gamblery111 <y18503950913@163.com>
2023-09-07 10:34:06 +0800

    90-读写inode

--- src/include/onix/fs.h
+++ src/include/onix/fs.h
@@ -46,12 +46,12 @@ typedef struct inode_t
     idx_t nr;             // i 节点号
     u32 count;            // 引用计数
     time_t atime;         // 访问时间
-    time_t ctime;         // 创建时间
+    time_t ctime;         // 修改时间
     list_node_t node;     // 链表结点
     dev_t mount;          // 安装设备
 } inode_t;
 
-typedef struct super_desc_t //超级块描述符
+typedef struct super_desc_t // 超级块描述符
 {
     u16 inodes;        // 节点数
     u16 zones;         // 逻辑块数
@@ -63,7 +63,7 @@ typedef struct super_desc_t //超级块描述符
     u16 magic;         // 文件系统魔数
 } super_desc_t;
 
-typedef struct super_block_t //文件系统的超级块
+typedef struct super_block_t // 文件系统的超级块
 {
     super_desc_t *desc;              // 超级块描述符
     struct buffer_t *buf;            // 超级块描述符 buffer
@@ -100,5 +100,11 @@ void iput(inode_t *inode);          // 释放 inode
 
 inode_t *named(char *pathname, char **next); // 获取 pathname 对应的父目录 inode
 inode_t *namei(char *pathname);              // 获取 pathname 对应的 in
+
+// 从 inode 的 offset 处，读 len 个字节到 buf
+int inode_read(inode_t *inode, char *buf, u32 len, off_t offset);
+
+// 从 inode 的 offset 处，将 buf 的 len 个字节写入磁盘
+int inode_write(inode_t *inode, char *buf, u32 len, off_t offset);
 
 #endif
